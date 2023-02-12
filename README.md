5544d6274bfbf9eacdb16ae10d5218221640f25e
avionics18 <avimurmu18@gmail.com>
2023-02-13 02:34:42 +0530

    project done

---
+++ README.md
@@ -0,0 +1,6 @@
+![Fauxica](project.jpg)
+
+# Fauxica
+
+- Web Design using SASS
+- Actual Design by DesignCourse Youtube Channel: [Video Link](https://www.youtube.com/watch?v=QwSN4n2sjR8)
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+// Initialize Modules
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+	return src('src/scss/style.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([cssnano()]))
+		.pipe(dest('public/css', { sourcemaps: '.' }));
+}
+
+// BrowserSync
+function bsServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: './public',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+
+	cb();
+}
+
+function bsReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+
+// ===GULP WORKFLOW===
+// Watch Task
+function watchTask() {
+	watch('public/*.html', bsReload);
+	watch(
+		['src/scss/**/*.scss'],
+		series(scssTask, bsReload)
+	);
+}
+
+// Default Gulp Task
+exports.default = series(
+	scssTask,
+	bsServe,
+	watchTask
+);
